5a9e8ed84ae35ae08010ba27be58cdfcd79f474a
mcrkvena <57915845+mcrkvena@users.noreply.github.com>
2022-02-09 16:31:43 +0100

    promjena porta za heroku

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-const port = 5000;
+const port = process.env.PORT;
 const server = http.createServer(express)
 const wss = new WebSocket.Server({server});
 
